7ea3c969c178c0a537c02e830e2e6ebc3d801117
Prabin Panta <pantaprabin30@gmail.com>
2024-12-30 22:02:00 +0545

    Remove unused React import from AdminLayout and AdminNavbar; clean up code

--- src/components/admin/layout/AdminLayout.tsx
+++ src/components/admin/layout/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { AdminNavbar } from './AdminNavbar';
 import { Dashboard } from '../../../pages/admin/Dashboard';
